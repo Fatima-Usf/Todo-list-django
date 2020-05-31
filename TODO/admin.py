91dc8b80a1f1c907b4f946f9c50d9066d3ffb2f9
Fatima-Usf <f.ucf.eva@gmail.com>
2020-05-31 05:28:22 +0100

    update

--- TODO/admin.py
+++ TODO/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import ItemTodo
 # Register your models here.
+
+class todoitems_admin(admin.ModelAdmin):
+    list_display=('content')
+
+admin.site.register(ItemTodo, todoitems_admin)
--- TODO/views.py
+++ TODO/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ItemTodo
+
 # Create your views here.
 
 def todoPlaningView(request):
-    return render(request, 'todo.html')
+    my_items = ItemTodo.objects.all()
+    return render(request, 'todo.html', {'my_todo_items':my_items})
